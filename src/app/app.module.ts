09f009b3fbf3ddcb0749ed6a660f59eb1af7f4d0
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-09-28 12:13:14 -0400

    Add manual router example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+
+
+const myRoutes: Routes = [
+    // "path" is the URL
+    { path: '', component: HomeComponent },
+         // /
+
+    { path: 'about', component: AboutComponent },
+         // /about
+
+    { path: 'contact-us', component: ContactUsComponent },
+         // /contact-us
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
